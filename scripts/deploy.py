85e4d07facc4f2a126463ea6226d4509de49e2b6
Ubuntu <raadim@Ubuntun-Server-20.naoo3azbuljujmj4wnanz0hdsg.ax.internal.cloudapp.net>
2021-11-23 19:47:16 +0000

    Check if development network and include Infura

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,8 +1,8 @@
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 
 def deploy_simple_storage():
-    account = accounts[0]
+    account = get_account()
     # account = accounts.add(config["wallets"]["from_key"]) # this is safer way of storing private key
     simple_storage = SimpleStorage.deploy({"from": account})
     stored_value = simple_storage.retrieve()
@@ -11,6 +11,13 @@ def deploy_simple_storage():
     transaction.wait(1)
     updated_stored_value = simple_storage.retrieve()
     print(updated_stored_value)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
